88e09b63fe2f1bf837e90eaa1530c75c049445a1
GuilletJoseph <guilletjoseph85@gmail.com>
2022-10-15 20:01:22 +0200

    ff

--- src/entites/TestAdressePostale.java
+++ src/entites/TestAdressePostale.java
@@ -5,8 +5,8 @@ public class TestAdressePostale {
 	
 	public static void main (String[] args) {
 		
-		
-		
+		AdressePostale ap1=new AdressePostale();
+		AdressePostale ap2=new AdressePostale();
 		
 	}
 }
---
+++ src/entites/TestPersonne.java
@@ -0,0 +1,13 @@
+package entites;
+
+import entites2.Personne;
+
+public class TestPersonne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Personne p1=new Personne();
+Personne p2=new Personne();
+	}
+
+}
